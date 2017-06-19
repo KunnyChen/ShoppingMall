c765f3b30ca06b93edaa2cdd128f6dac880119b2
KunnyChen <abc582700457@qq.com>
2017-06-19 23:15:28 +0800

    back to top
    back to top

---
+++ Mall/js/script.js
@@ -0,0 +1,33 @@
+/*--------------返回顶部----------------*/
+$(document).ready(function () {
+    var backButton = $('.back-to-top');
+    var topText = $('.back-to-top-text');
+    backButton.on('click', function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 500);
+        $(this).animate({
+            top: 0
+        }, function () {
+            var em = $(this);
+            em.css("opacity", 0);
+            em.animate({
+                top: "85%"
+            });
+            setTimeout(function (e) {
+                em.css("opacity", 1);
+            }, 500)
+        });
+    });
+    $(window).on('scroll', function () {
+        //console.log('back');
+        if ($(window).scrollTop() > $(window).height() * 0.6) {
+            backButton.fadeIn();
+            topText.show();
+        } else {
+            backButton.fadeOut();
+            topText.hide();
+        }
+    });
+    $(window).trigger('scroll');
+});
